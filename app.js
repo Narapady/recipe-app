e01d8928fbd117d2fe41879f31b55075f3575419
narapady <chhuoynarapady@yahoo.com>
2020-09-18 14:12:03 -0700

    final commit

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-const searchForm = document.querySelector('form');
-const searchResultDiv = document.querySelector('.search-result');
-const container = document.querySelector('.container');
-let searchQuery = '';
-
+// Get api info
 const appID = 'fff1588a';
 const appKey = 'da13a58bf65ccd4f18f656756117a4c4';
 
+// Selectors
+const searchForm = document.querySelector('form');
+const container = document.querySelector('.container');
+const resultDiv = document.querySelector('.search-result');
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   container.classList.remove('initial');
-  searchQuery = e.target.querySelector('input').value;
-  fetchData(searchQuery)
-    .then(data => generateHTML(data.hits))
+
+  // Get the input
+  const input = document.querySelector('form input').value;
+
+  // Fetch data from the api
+  fetchData(input)
+    .then(data => showItemsUI(data.hits))
     .catch(err => console.log(err));
 
   searchForm.reset();
-
 });
 
-
+// Async function fetching data from the api
 async function fetchData(item) {
-  const baseURL = `https://api.edamam.com/search?q=${item}&app_id=${appID}&app_key=${appKey}&to=20`;
-  const response = await fetch(baseURL);
+  const url = `https://api.edamam.com/search?q=${item}&app_id=${appID}&app_key=${appKey}&to=20`;
+
+  const response = await fetch(url);
   const data = await response.json();
+
   return data;
 }
 
-function generateHTML(results) {
+// Function to show in the UI
+function showItemsUI(results) {
   let html = '';
   results.forEach(result => {
     html += `
@@ -35,13 +41,14 @@ function generateHTML(results) {
       <img src=${result.recipe.image} alt="">
       <div class="flex-container">
         <h1 class="title">${result.recipe.label}</h1>
-        <a href=${result.recipe.url} target="_blank">View Recipe</a>
+        <a href=${result.recipe.url}>View Recipe</a>
       </div>
       <p class="item-data">Calories: ${Math.floor(result.recipe.calories)}</p>
-      <p class="item-data">Diet Label: ${result.recipe.dietLabels.length > 0? result.recipe.dietLabels : 'No Data Found'}</p>
-      <p class="item-data">Health Label: ${result.recipe.healthLabels}</p>
+      <p class="item-data">Diet Label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : 'Data Not Found' }</p>
+      <p class="item-data">heal Labels: ${result.recipe.healthLabels}</p>
     </div>
     `;
   });
-  searchResultDiv.innerHTML = html;
+
+  resultDiv.innerHTML = html;
 }
